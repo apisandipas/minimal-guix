0e27cbae2c94bf457c88c7a18a4d747045abbc4b
Bryan Paronto <bryan@kindsys.us>
2023-04-05 17:25:48 -0500

    init

---
+++ bryan/systems/atlas.scm
@@ -0,0 +1,29 @@
+(define-module (bryan systems atlas)
+  #:use-module (bryan systems base-system)
+  #:use-module (gnu))
+
+(operating-system
+ (inherit base-operating-system)
+ (host-name "atlas")
+
+ (swap-devices
+  (list (swap-space
+         (target
+          (file-system-label "swap-partition")))))
+
+ (file-systems (cons*
+                (file-system
+                 (device (file-system-label "system-root"))
+                 (mount-point "/")
+                 (type "ext4")
+                 (dependencies mapped-devices))
+                (file-system
+                 (device (file-system-label "system-home"))
+                 (mount-point "/home")
+                 (type "ext4")
+                 (dependencies mapped-devices))
+                (file-system
+                 (device (file-system-label "EFI_PART"))
+                 (mount-point "/boot/efi")
+                 (type "vfat"))
+                %base-file-systems)))
---
+++ channels.scm
@@ -0,0 +1,30 @@
+(list
+  (channel
+    (name 'rde)
+    (url "https://git.sr.ht/~abcdw/rde")
+    (branch "master")
+    (commit
+      "a7b59443405169600a00f0b295a3fb1de360cb0b")
+    (introduction
+     (make-channel-introduction "257cebd587b66e4d865b3537a9a88cccd7107c95"
+        (openpgp-fingerprint "2841 9AC6 5038 7440 C7E9  2FFA 2208 D209 58C1 DEB0"))))
+  (channel
+    (name 'flat)
+    (url "https://github.com/flatwhatson/guix-channel.git")
+    (branch "master")
+    (commit
+     "657da22f0229b978b7bf4e4d476f59f17f6a175f")
+    (introduction
+     (make-channel-introduction "b62ba3214ed0f781e2d6015044ae8a4a1bd5c7d7"
+       (openpgp-fingerprint "736A C00E 1254 378B A982  7AF6 9DBE 8265 81B6 4490"))))
+  (channel
+    (name 'nonguix)
+    (url "https://gitlab.com/nonguix/nonguix"))
+  (channel
+    (name 'guix)
+    (url "https://git.savannah.gnu.org/git/guix.git")
+    (branch "master")
+    (commit "04724e59971b03f86a410285653d24005c62b924")
+    (introduction
+     (make-channel-introduction "9edb3f66fd807b096b48283debdcddccfea34bad"
+       (openpgp-fingerprint "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
